1ceaf73c4caba839e8b9ee7bb56d900def5eef8b
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-07 17:39:18 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  count = 0
+  array.each do |number|
+    new_array.push(array[count] ** 2)
+    count += 1
+  end
+  return new_array
 end
