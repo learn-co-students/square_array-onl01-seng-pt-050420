90cff4558917f22c0640d69bf1a91a46f76f8135
Valentina Panic <valentina.panic87@gmail.com>
2020-05-09 03:50:07 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
+def square_array(arrays)
+new_numbers = []
+arrays.each do |x|
+    new_numbers.push (x * x)
+  end
+new_numbers
 end
+
+#def square_array(new_numbers)
+#  new_numbers. do {|x| x * x }
+#  end
+
+#end
