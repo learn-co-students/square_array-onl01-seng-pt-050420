d41162f86f820de1c87ca21705232b771a885c60
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-11 17:28:24 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+require "pry"
+# def square_array(array)
+#   array.collect do |num|
+#   num * num
+# end
+# end
+
 def square_array(array)
-  # your code here
+  square = []
+  
+  array.each do |num|
+   square << num * num
+  end
+  square
 end
