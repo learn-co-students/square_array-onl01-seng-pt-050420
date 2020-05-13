11ee8f872282e4924b708fd3d84f786e4ad7cfe7
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-13 22:08:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
+require "pry"
+def square_array(numbers)
+  new_array = []
+  numbers.each do |number|
+  new_array << number * number
+  end
+  return new_array
 end
+# def square_array(numbers)
+  
+#   numbers.collect do |number|
+#     number * number
+#   end
+ 
+# end
