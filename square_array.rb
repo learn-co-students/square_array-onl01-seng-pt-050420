fd5bf5441bac392c680df19acb31867074532db3
reyemarv <reyemarv@gmail.com>
2020-06-01 18:11:43 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each{|num| new_array << num ** 2}
+  new_array 
+  # array.map{|num| num ** 2} (alternative and preffered)
 end
